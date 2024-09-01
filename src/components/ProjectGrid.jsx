b36f423ede00b549437fb2b14e317fd42f399b17
SIDHARTH RAMANATHAN <sidhuram365@gmail.com>
2024-09-01 21:51:17 +0530

    ADDED APPLICATION DEV

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -1,79 +1,60 @@
+import { FlutterProjects } from "../data/Projects";
+
+import PlayStoreIcon from "../assets/icons/playstore.png";
+import AppStoreIcon from "../assets/icons/appstore.png";
+
 export default function ProjectGrid() {
   return (
-    <div class="absolute p-7 z-10 top-0 grid grid-cols-1 md:grid-cols-3 gap-4">
-      <div class="grid gap-4">
-        <div className="flex flex-col max-h-full">
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-            alt=""
-          />
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div class="grid gap-4">
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
-            alt=""
-          />
-        </div>
-      </div>
-      <div class="grid gap-4">
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          <img
-            class="h-auto max-w-full rounded-lg"
-            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
-            alt=""
-          />
-        </div>
+    <div className="absolute z-10 md:p-7 p-4 top-0 w-screen h-full ">
+      <div className="masonry-grid">
+        {FlutterProjects.map((project) => (
+          <div
+            key={project.id}
+            className="masonry-item mb-7  p-5 bg-opacity-0 rounded-2xl flex flex-col"
+            style={{
+              backgroundColor: project.color,
+            }}
+          >
+            <div className="flex flex-row gap-5 items-start">
+              <img
+                className="h-10 w-10 object-cover rounded-md shadow-lg"
+                src={project.icon}
+                alt={project.title}
+              />
+              <h3 className="font-ThunderBold pt-3 text-start text-[32px] leading-7 tracking-wide">
+                {project.title}
+              </h3>
+            </div>
+
+            <div className="pt-4 flex flex-row gap-4">
+              {project.androidLink && (
+                <a
+                  href={project.androidLink}
+                  className="hover:scale-110 duration-300"
+                >
+                  <img
+                    src={PlayStoreIcon}
+                    alt="Play Store"
+                    className="h-8 w-8"
+                  />
+                </a>
+              )}
+
+              {project.iosLink && (
+                <a
+                  href={project.iosLink}
+                  className="hover:scale-110 duration-300"
+                >
+                  <img src={AppStoreIcon} alt="App Store" className="h-8 w-8" />
+                </a>
+              )}
+            </div>
+
+            <p className="pt-5 font-GeneralSansRegular text-start text-[15px] tracking-wide leading-7">
+              {project.description}
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
---
+++ src/data/Projects.js
@@ -0,0 +1,85 @@
+import strollinIcon from "../assets/icons/strollin.png";
+import rickkrIcon from "../assets/icons/rickkr.png";
+import kingsClubIcon from "../assets/icons/kingsclub.png";
+import brainsilIcon from "../assets/icons/brainsil.png";
+import flystoreIcon from "../assets/icons/flystore.png";
+import flametechovensIcon from "../assets/icons/flametechovens.png";
+import athmakalariIcon from "../assets/icons/athmakalari.png";
+const FlutterProjects = [
+  {
+    icon: strollinIcon,
+    title: " Strollin: A Multi-Vendor Ecommerce",
+    description:
+      " Real-Time Analytics Dashboard: Provides the admin with comprehensive, real time insights into vendor performance, sales trends, and customer behavior, enabling data-driven decision-making.Vendor and User Management: Allows the admin to efficiently manage vendorand user accounts, including adding, editing, and removing profiles, ensuring smooth operations.Product and Order Management: Enables the admin to oversee productl istings and monitor order statuses across all vendors, facilitating efficientinventory control and issue resolution.",
+    androidLink: "",
+    iosLink: "",
+    color: "#f57184",
+  },
+  {
+    icon: rickkrIcon,
+    title: " Rickkr - Reel & Ecommerce",
+    description:
+      " E-commerce & Admin Expertise: Built the e-commerce section and admin panel for Rickker, a reels-shopping app. Implemented features like product listings, search, checkout, and full admin functionalities for product/order management & analytics. Modern Tech Stack: Utilized Flutter for cross-platform development, Firebase for backend services, and Bloc with Freezed for efficient state management. Collaborative Development: Successfully collaborated with a teammate on the reels section, showcasing strong teamwork skills",
+    androidLink: "https://play.google.com/store/apps/details?id=com.rickkr",
+    iosLink: "https://apps.apple.com/in/app/rickkr/id6502875485",
+    websiteLink: "",
+    color: "#b771f5",
+  },
+  {
+    icon: kingsClubIcon,
+    title: "Kings Club - Education & Chat App",
+    description:
+      " Built Kings Club, a real-time educational chat app using Flutter for a user friendly experience and Firebase for scalable functionality.  Implemented user management and admin features, enabling broadcast  messaging and user base control.  Overcame challenges in the chat section to deliver a successfull solution. This  project highlights my skills in building real-time chat apps, user management,  and Firebase integration",
+    androidLink:
+      "https://play.google.com/store/apps/details?id=anilbalachandran.kingsclub&pli=1",
+    iosLink:
+      "https://totalx.in/.https:/apps.apple.com/us/app/kings-club/id6479393707",
+    websiteLink: "https://thekingmakeruniversity.com/",
+    color: "#f5ba71",
+  },
+  {
+    icon: brainsilIcon,
+    title: "BrainSil-  Learning Platform ",
+    description:
+      "User-Friendly Course Navigation: Intuitive interface that allows users to easily browse courses and seamlessly connect with instructors, enhancing the learning experience. AI-Powered Personalized Learning: Integrated E Sha AI, utilizing OpenAI's API, to provide personalized learning paths and assistance, tailoring content to individual student needs. Structured Learning with Certification: Section-wise course progression with built-in exams, enabling users to track their learning journey and earn certificates upon successful completion. Enhanced Content Protection: Implemented screen recording restrictions using Kotlin code, ensuring the security and integrity of course content against unauthorized recording.",
+    androidLink: "https://play.google.com/store/apps/details?id=com.brainsil",
+    iosLink: "https://apps.apple.com/in/app/brainsil/id6480062798",
+    websiteLink: "",
+    color: "#6e97f5",
+  },
+  {
+    icon: flystoreIcon,
+    title: " Fly Store - Super App",
+    description:
+      "Collaboratively developed Fly Store, a multi-function app using Flutter for a seamless experience (OLX, taxi booking, jobs). Integrated complex features (buying/selling, taxi booking, job listings) within a single platform. Leveraged Flutter and Firebase for efficient development and tackled challenges through teamwork",
+    androidLink:
+      "https://play.google.com/store/apps/details?id=flystore.flystore",
+    iosLink: "https://apps.apple.com/us/app/flystore/id6478517933",
+    websiteLink: "",
+    color: "#fa4d4d",
+  },
+  {
+    icon: athmakalariIcon,
+    title: "Athma Kalari - Education",
+    description:
+      "Built Athma Kalari, a cross-platform e-learning platform for Kalaripayattu (mobile app with Flutter & Firebase, website with HTML, CSS & JavaScript). Designed a user-friendly mobile app (Flutter & Firebase) with Provider for state management, enabling students to seamlessly access online classes. Constructed a comprehensive admin panel (Flutter & Firebase) for educators to manage content, users, and functionalities.",
+    androidLink:
+      "https://play.google.com/store/apps/details?id=com.athma_kalari",
+    iosLink: "",
+    websiteLink: "",
+    color: "#7ef784",
+  },
+  {
+    icon: flametechovensIcon,
+    title: "Flame Tech Ovens - B2B Application",
+    description:
+      " Built Flame Tech Ovens, a multi-level B2B app with Flutter and Firebase.  Designed separate admin interfaces for Super Admins (managing everything)  and Branch Admins (focusing on inventory, orders, and communication).  Users browse ovens, place orders, and track purchases through a user friendly app",
+    androidLink:
+      "https://play.google.com/store/apps/details?id=com.flametechovens.flametechovens",
+    iosLink: "",
+    websiteLink: "",
+    color: "#f7e17e",
+  },
+];
+
+export { FlutterProjects };
